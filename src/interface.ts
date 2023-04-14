56c3b17ef87766a8b7a02a02e2fdac10ed7b63b4
Ramanand Tiwari <112823601+Ramanand1101@users.noreply.github.com>
2023-04-15 01:36:50 +0530

    interface.js

--- src/interface.ts
+++ src/interface.ts
@@ -2,44 +2,60 @@
 // ham interface ka use karte hai 
 // jaise ki ham example le lete hai 
 
+
+//Proper definition-
+/* When defining the signature of an object, you will usually use an interface. This is useful 
+if we need to check that multiple objects have the same specific properties and value types: */
+
+
+
 /* Example-1 */
-let person1 = {
-    name: "Ramanand",
-    subject: "MERN",
-    class: 12,
-    isProgrammer: true
-};
-let person2 = {
-    name: "Rahul",
-    subject: "MERN",
-    class: 12,
-    isProgrammer: true
-};
-console.log(person1.name);
-console.log(person2.name);
-
-/* output-Ramanand
-          Rahul */
-
-
-
-          
-/* Example-2 */
-function printCoord(pt) {
-    console.log("The coordinate's x value is " + pt.x);
-    console.log("The coordinate's y value is " + pt.y);
+interface person{
+    name:string,
+    subject:string,
+    class:number ,
+    isProgrammer:boolean
 }
+
+let person1:person={
+    name:"Ramanand",
+    subject:"MERN",
+    class:12,
+    isProgrammer:true
+
+};
+
+let person2:person={
+    name:"Rahul",
+    subject:"MERN",
+    class:12,
+    isProgrammer:true
+}
+console.log(person1.name)
+console.log(person2.name)
+
+
+
+/* Example-2 */
+interface Point {
+  x: number;
+  y: number;
+}
+
+function printCoord(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
 printCoord({ x: 100, y: 100 });
-/* output-The coordinate's x value is 100
-The coordinate's y value is 100 */
-
-
 
 /* Example-3 */
-function sonfather(data) {
-    console.log(`Son height is similar to father height i.e ${data.height}inch`);
-    console.log(`Son weight is similar to father weight i.e ${data.weight}kg`);
+interface dad{
+    height:number,
+    weight:number
 }
-sonfather({ height: 270, weight: 70 });
-/* Output- Son height is similar to father height i.e 270inch
-Son weight is similar to father weight i.e 70kg*/
+function sonfather(data:dad){
+    console.log(`Son height is similar to father height i.e ${data.height}inch`)
+    console.log(`Son weight is similar to father weight i.e ${data.weight}kg`)
+}
+sonfather({height:270,weight:70})
